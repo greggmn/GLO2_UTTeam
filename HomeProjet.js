5e066a39923fe55feb6bfa88b381a2cf43fedb51
greggmn <gregoire.gaumain@gmail.com>
2023-11-29 17:02:34 +0100

    Greg

--- HomeProjet.js
+++ HomeProjet.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env ts-node
 
 
-const { program } = require('@caporal/core');
+const {program} = require('@caporal/core');
 const readlineSync = require('readline-sync');
 program
     .command('accueil', 'Enonce les options du programme')
     .action(() => {
+
+        const question1 = 'Entrer votre mot de passe \n';
+        const reponse2 = readlineSync.question(question1);
+
+
         console.log("Voici les options du programme:")
         console.log("Taper 1 pour vérifier la validité du format CRU")
         console.log("Taper 2 pour consulter les salles disponibles selon le créneau horaire")
@@ -16,52 +21,53 @@ program
         console.log("Taper 7 pour annuler une réservation")
 
         //Si admin
-        console.log("Taper 8 pour visualiser l’occupation des salles")
-        console.log("Taper 9 pour créer et/ou modifier une salle")
+        if (reponse2 === "admin") {
+            console.log("Taper 8 pour visualiser l’occupation des salles")
+            console.log("Taper 9 pour créer et/ou modifier une salle")
 
-        const question= '\n Entrer le numéro de la fonction à utiliser \n';
+        }
+        const question = '\n Entrer le numéro de la fonction à utiliser \n';
         const reponse = readlineSync.question(question);
 
-        switch (reponse){
-                case '1':
+        switch (reponse) {
+            case '1':
 
-                        break;
-                case '2':
+                break;
+            case '2':
 
-                        break;
-                case '3':
+                break;
+            case '3':
 
-                        break;
-                case '4':
+                break;
+            case '4':
 
-                        break;
-                case '5':
+                break;
+            case '5':
 
-                        break;
-                case '6':
+                break;
+            case '6':
 
-                        break;
-                case '7':
+                break;
+            case '7':
 
-                        break;
-                case '8':
+                break;
+            case '8':
+                if (reponse2 !== "admin") {
+                    console.log("Accès interdit");
+                }
 
-                        break;
-                case '9':
-
-                        break;
+                break;
+            case '9':
+                if (reponse2 !== "admin") {
+                    console.log("Accès interdit");
+                }
+                break;
 
 
         }
 
 
-
-
-
     });
-
-
-
 
 
 program.run();
