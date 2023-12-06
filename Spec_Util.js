5734dd73ddd8f4d3ad0d5ef4505897ab80f0e318
mathys-lemort <lemormath@gmail.com>
2023-12-06 14:19:51 +0100

    ajout des fonctions

--- Spec_Util.js
+++ Spec_Util.js
@@ -75,6 +75,33 @@ program
     }
 });
 
+function voirInfosSalles(donnees) {
+    let infosSalles = {}; // Créer un objet pour stocker les infos par salle
+
+    // Remplir l'objet avec les données
+    donnees.forEach(ue => {
+        ue.creneaux.forEach(creneau => {
+            if (!infosSalles[creneau.salle]) {
+                infosSalles[creneau.salle] = [];
+            }
+            infosSalles[creneau.salle].push({
+                NbPlaces: creneau.nbplaces,
+                Jour: creneau.jour,
+                Horaire: creneau.horaire,
+                GroupeCours: creneau.groupe_cours
+            });
+        });
+    });
+
+    // Afficher les informations
+    Object.keys(infosSalles).forEach(salle => {
+        console.log(`Salle: ${salle}`);
+        infosSalles[salle].forEach(creneau => {
+            console.log(`    NbPlaces: ${creneau.NbPlaces}, Jour: ${creneau.Jour}, Horaire: ${creneau.Horaire}, GroupeCours: ${creneau.GroupeCours}`);
+        });
+    });
+}
+
 program.run();
 
 function calculerTauxOccupation(donnees) {
@@ -117,5 +144,29 @@ program
         logger.error("Erreur lors de l'affichage des salles par taux d'occupation :", err);
     }
 });
+
+function voirTauxOccupation(donnees) {
+    const heuresOuvertureParJour = 12; // de 8h à 20h
+    const joursOuvrables = 5; // Supposons qu'il y a 5 jours ouvrables dans une semaine
+    let tauxOccupationSalles = {};
+
+    donnees.forEach(ue => {
+        ue.creneaux.forEach(creneau => {
+            if (!tauxOccupationSalles[creneau.salle]) {
+                tauxOccupationSalles[creneau.salle] = 0;
+            }
+            if (creneau.horaire) { // Vérifier si le champ horaire existe
+                let [heureDebut, heureFin] = creneau.horaire.split('-').map(heure => parseFloat(heure));
+                tauxOccupationSalles[creneau.salle] += heureFin - heureDebut;
+            }
+        });
+    });
+
+    Object.keys(tauxOccupationSalles).forEach(salle => {
+        tauxOccupationSalles[salle] = (tauxOccupationSalles[salle] / (heuresOuvertureParJour * joursOuvrables)) * 100;
+    });
+
+    return Object.entries(tauxOccupationSalles).sort((a, b) => b[1] - a[1]);
+}
 
 program.run();
