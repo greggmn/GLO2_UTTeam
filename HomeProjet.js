970e0aea07e16f526d332340f96057dc33fcbb13
TheoKLR <theo.koehler@orange.fr>
2023-12-06 14:05:03 +0100

    MAJ reservation + menu

--- HomeProjet.js
+++ HomeProjet.js
@@ -1,73 +1,191 @@
 #!/usr/bin/env ts-node
-
-
-const {program} = require('@caporal/core');
 const readlineSync = require('readline-sync');
-program
-    .command('accueil', 'Enonce les options du programme')
-    .action(() => {
 
-        const question1 = 'Entrer votre mot de passe \n';
-        const reponse2 = readlineSync.question(question1);
+import('inquirer')
+  .then((inquirerModule) => {
+    const inquirer = inquirerModule.default;
+
+function demarrage(){
+    const inscription = [
+        "administrateur",
+        "professeur",
+        "élève"
+    ];
+
+    return inquirer.prompt([
+        {
+            type: 'rawlist',
+            name: 'choix',
+            message: "Votre statut dans l'université:",
+            choices: inscription
+        },
+    ]) ;
+}
+    
+
+function actionAFaire(){
+    const rawList = [
+        "vérifier la validité du format CRU",
+        "consulter les salles disponibles selon le créneau horaire",
+        "consulter les disponibilités d’une salle",
+        "consulter les informations d'une salle",
+        "réserver une salle",
+        "voir vos réservations",
+        "annuler une réservation"
+      ];
+
+      return inquirer.prompt([
+        {
+            type: 'rawlist',
+            name: 'choix',
+            message: "Choisissez l'action que vous souhaitez réaliser:",
+            choices: rawList
+          },
+    ]) ;
+}
+
+function actionAFaireAdmin(){
+    const rawList = [
+        "vérifier la validité du format CRU",
+        "consulter les salles disponibles selon le créneau horaire",
+        "consulter les disponibilités d’une salle",
+        "consulter les informations d'une salle",
+        "réserver une salle",
+        "voir vos réservations",
+        "annuler une réservation",
+        "visualiser l’occupation des salles",
+        "créer et/ou modifier une salle"
+      ];
+
+      return inquirer.prompt([
+        {
+            type: 'rawlist',
+            name: 'choix',
+            message: "Choisissez l'action que vous souhaitez réaliser:",
+            choices: rawList
+          },
+    ]) ;
+}
 
 
-        console.log("Voici les options du programme:")
-        console.log("Taper 1 pour vérifier la validité du format CRU")
-        console.log("Taper 2 pour consulter les salles disponibles selon le créneau horaire")
-        console.log("Taper 3 pour consulter les disponibilités d’une salle")
-        console.log("Taper 4 pour consulter les informations d'une salle")
-        console.log("Taper 5 pour réserver une salle")
-        console.log("Taper 6 pour voir vos réservations")
-        console.log("Taper 7 pour annuler une réservation")
+let statut, action, identifiant;
+demarrage()
+    .then((reponseDemarrage) => {
+        statut = reponseDemarrage.choix;
 
-        //Si admin
-        if (reponse2 === "admin") {
-            console.log("Taper 8 pour visualiser l’occupation des salles")
-            console.log("Taper 9 pour créer et/ou modifier une salle")
+        const question1 = 'Entrez votre identifiant \n';
+        identifiant = readlineSync.question(question1);
 
+        if (statut === "administrateur")
+            return actionAFaireAdmin();
+        else
+            return actionAFaire();
+    })
+    .then((reponseAction) => {
+        action = reponseAction.choix;
+
+        switch(action){
+            case "vérifier la validité du format CRU":
+
+                break;
+            case "consulter les salles disponibles selon le créneau horaire":
+
+                break;
+            case "consulter les disponibilités d’une salle":
+
+                break;
+            case "consulter les informations d'une salle":
+
+                break;
+            case "réserver une salle":
+                const reservation = require('./reservation');
+                reservation.menuReservation(statut, identifiant);
+                break;
+            case "voir vos réservations":
+
+                break;
+            case "annuler une réservation":
+
+                break;
+            case "visualiser l’occupation des salles":
+
+                break;
+            case "créer et/ou modifier une salle":
+
+                break;
         }
-        const question = '\n Entrer le numéro de la fonction à utiliser \n';
-        const reponse = readlineSync.question(question);
 
-        switch (reponse) {
-            case '1':
-
-                break;
-            case '2':
-
-                break;
-            case '3':
-
-                break;
-            case '4':
-
-                break;
-            case '5':
-
-                break;
-            case '6':
-
-                break;
-            case '7':
-
-                break;
-            case '8':
-                if (reponse2 !== "admin") {
-                    console.log("Accès interdit");
-                }
-
-                break;
-            case '9':
-                if (reponse2 !== "admin") {
-                    console.log("Accès interdit");
-                }
-                break;
-
-
-        }
-
-
+    })
+    .catch((erreur) => {
+        console.log('Erreur :', erreur);
     });
 
+  })
 
-program.run();
+
+
+/*
+
+program
+    .command("Option3", "Execute l'option 3 (disponibilités d’une salle)")
+    .argument("<salle>", "salle dont on veut les disponibilités")
+    .argument("<folder>", "nom du dossier contenant les fichiers")
+    .action(async ({ logger, args }) => {
+        const folderName = args.folder;
+        const folderPath = path.join(__dirname, folderName);
+
+        logger.info(`Lecture des fichiers dans le dossier ${folderPath}`);
+
+        // Lire le contenu du dossier
+        const files = fs.readdirSync(folderPath);
+
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+
+            try {
+                logger.info(`Lecture du fichier ${filePath}`);
+                const data = fs.readFileSync(filePath, 'utf8');
+
+                // Exécuter l'analyse
+                analyzer.parse(data);
+
+                const salle = args.salle;
+                const disponibilites = [];
+
+                analyzer.parsedUE.forEach((ue) => {
+                    ue.creneaux.forEach((creneau) => {
+                        if (creneau.salle === salle) {
+                            disponibilites.push({
+                                horaire: creneau.horaire,
+                                jour: creneau.jour,
+                            });
+                        }
+                    });
+                });
+
+                const uniqueDisponibilites = [...new Set(disponibilites)];
+
+                if (uniqueDisponibilites.length > 0) {
+                    logger.info(`Disponibilités pour la salle ${args.salle} dans le fichier ${file}:`);
+                    uniqueDisponibilites.forEach(({ horaire, jour }) => {
+                        logger.info(`- ${jour} ${horaire}`);
+                    });
+                } else {
+                    logger.info(`Aucune disponibilité trouvée pour la salle ${args.salle} dans le fichier ${file}.`);
+                }
+            } catch (err) {
+                // Gérer les erreurs éventuelles ici
+                logger.error(`Erreur lors de la lecture du fichier ${file}: ${err.message}`);
+            }
+        }
+
+        // Afficher un message une fois toutes les analyses terminées
+        logger.info("Toutes les analyses sont terminées.");
+    });
+*/
+/*
+program.run;
+
+
+
+program.run();*/
---
+++ reservation.js
@@ -0,0 +1,104 @@
+
+const Creneau = require('./Creneau.js');
+const Ue = require('./UE.js');
+const creneau = require('./Creneau.js');
+const readlineSync = require('readline-sync');
+
+var menuReservation = function(statut, identifiant){
+
+    console.log("Tapez 1 pour réserver une salle ");
+    console.log("Tapez 2 pour réserver voir vos réservations");
+    const choixAction = 'Choix de réservation: \n';
+    const reponseAction = readlineSync.question(choixAction);
+
+    if (reponseAction === "2"){
+        mesDispos();
+    }
+    else if (reponseAction === "1"){
+
+        if (statut === "prof"){
+
+            let reponse;
+            let nomUe;
+            do{
+                console.log("Tapez 1 pour réserver la salle sur un créneau particulier");
+                console.log("Tapez 2 pour réserver la salle tout un semestre");
+                const choix = "Choix de réservation: \n";
+                reponse = readlineSync.question(choix); 
+                if (reponse!=="1" && reponse!=="2")
+                    console.log("Choix non valide");
+                
+                const saisieUe = "Entrez le nom de l'UE pour laquelle vous réservez la salle: \n";
+                nomUe = readlineSync.question(saisieUe);
+                if (nomUe.match(/[A-Z]{1,4}\d{0,3}/)===null)
+                    console.log("UE saisie non valide");
+            } while((reponse!=="1" | reponse!=="2")&&(nomUe.match(/[A-Z]{1,4}\d{0,3}/)===null))
+
+        }
+
+        let jour;
+        do{
+            const choixJour = 'Entrez le jour pour lequel vous voulez réserver une salle (ex: L, MA, ME)\n';
+            jour = readlineSync.question(choixJour);
+            matched = jour.match(/(L|MA|ME|J|V|S)/);
+            if (matched === null)
+                console.log("Jour saisi invalide");
+        } while(matched === null)
+        
+        let horaire;
+        do{
+            const choixHoraire = "Entrez l'heure à laquelle vous voulez réserver une salle (ex: 10:00 - 12:00) \n";
+            horaire = readlineSync.question(choixHoraire);
+            matched = horaire.match(/\d{1,2}:\d{2}-\d{1,2}:\d{2}/)
+            if (matched === null)
+                console.log("Horaire saisi invalide");
+        } while(matched===null)
+        
+
+        //Fonction de Gregoire(jour, horaire) qui doit afficher toutes les dispos et retourner un tableau des créneaux dispos avec les UE
+
+        let salle;
+        do {
+            const choixSalle = "Entrez le nom de la salle à réserver \n";
+            salle = readlineSync.question(choixSalle);
+            var test = testDispo(salle, horaire, jour,allCreneaux);
+        } while (test === false)
+
+        
+        if (reponse === "1"){
+            reserver(salle, jour, horaire, nomUe);
+        } else if (reponse === "2"){
+            reserverSemestre(salle, jour, horaire, nomUe);
+        }
+
+            
+        if (statut === "eleve" | "élève"){
+            reserver(salle, jour, horaire,"Réservation");
+        }
+
+    }
+
+}
+
+
+var reserver = function(nomSalle, jour, horaire, nomUe){
+    creneau = new Creneau("R0", "0", jour, horaire, "F1", nomSalle);
+    ue = new Ue(nomUe, [creneau]);
+}
+
+
+var reserverSemestre = function (nomSalle, jour, horaire,nomUe){
+    creneau = new Creneau("R0", "0", jour, horaire, "F1", nomSalle);
+    ue = new Ue(nomUe, [creneau]);
+}
+
+var testDispo = function(nomSalle, horaire, jour, allCreneaux){
+    let creneauExiste = allCreneaux.some(creneau => creneau.salle === nomSalle & creneau.horaire === horaire & creneau.jour === jour);
+    return creneauExiste;
+}
+
+var mesDispos = function(){
+
+}
+
+menuReservation("prof");
